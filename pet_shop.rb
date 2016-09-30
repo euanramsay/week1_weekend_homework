3c8fe6a8a91ae6f9ba8607951c52a39da60ba836
euanramsay <euancramsay@aol.com>
2016-09-30 14:14:50 +0100

    Added def add_or_remove_cash(pet_shop, amount)

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,2 +1,14 @@
 require('pry-byebug')
 
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]  
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  return pet_shop[:admin][:total_cash] += amount
+end
+
