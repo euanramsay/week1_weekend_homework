2723be1f3c3ecf81c0d00bdeba83f5a153b8643d
euanramsay <euancramsay@aol.com>
2016-09-30 14:20:58 +0100

    Added def increase_pets_sold(pet_shop, amount_of_pets)

--- pet_shop.rb
+++ pet_shop.rb
@@ -16,4 +16,6 @@ def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
 
-
+def increase_pets_sold(pet_shop, amount_of_pets)
+  return pet_shop[:admin][:pets_sold] += amount_of_pets
+end
