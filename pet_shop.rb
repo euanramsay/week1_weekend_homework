2c2b81505c27ef03835f4dca7441cdfa09fa808a
euanramsay <euancramsay@aol.com>
2016-09-30 14:24:16 +0100

    Added def stock_count(pet_shop)

--- pet_shop.rb
+++ pet_shop.rb
@@ -19,3 +19,7 @@ end
 def increase_pets_sold(pet_shop, amount_of_pets)
   return pet_shop[:admin][:pets_sold] += amount_of_pets
 end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
